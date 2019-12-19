774ec21e9d6229588c082d1c28d866d042334239
Scott Hoisington <hoyzington@gmail.com>
2019-12-19 00:17:52 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,15 +3,18 @@ require 'pry'
 class String
 
   def sentence?
-    
+    return true if self.end_with?(".")
+    false
   end
 
   def question?
-
+    return true if self.end_with?("?")
+    false
   end
 
   def exclamation?
-
+    return true if self.end_with?("!")
+    false
   end
 
   def count_sentences
